e2084ee3fda1fb69914f055f7a2d91a13167ec58
Singh Virendra <virendrasingh@Virendras-MacBook.local>
2026-03-01 13:21:39 +0530

    chore: allow Vercel CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://movie-flix-final.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
